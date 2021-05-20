53e02cf6f143b849df3d76150ddae0366d0d34db
gbenaimako <gbenimako@gmail.com>
2021-05-21 00:38:52 +0200

    cart image modified

--- js/index.js
+++ js/index.js
@@ -192,8 +192,7 @@ async function displayCart() {
     return (cartOutput += `
     <div class="cart-row">
     <div class="cart-item cart-column">
-        <img class="cart-item-image"
-        src="${item.image}" width="100" height="100">
+
         <span class="cart-item-title">${item.name}</span>
     </div>
     <span class="cart-price cart-column">${item.price}</span>
